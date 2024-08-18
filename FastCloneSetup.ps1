a515715ede4d250b09a68f49c314c6b2cf513a1a
rucrov <desueted@inbox.ru>
2024-08-18 23:30:48 +0500

    add setup script, fix readme

---
+++ FastCloneSetup.ps1
@@ -0,0 +1,12 @@
+param (
+    [string]$Neko
+)
+
+$CallPath = Get-Location
+New-Item "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone"
+if ($Neko -eq "neko") {
+    New-ItemProperty "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone" -Name "Icon" -Value "$CallPath\NekoArk.ico"
+} else {
+    New-ItemProperty "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone" -Name "Icon" -Value "$CallPath\FastClone.ico"
+}
+New-Item "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone\command" -Value "powershell.exe -ExecutionPolicy Bypass -File $CallPath\FastClone.ps1"
