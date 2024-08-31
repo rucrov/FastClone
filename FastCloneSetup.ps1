8d5542b23ca25b0fd1dff2dc06d984c63324305c
rucrov <desueted@inbox.ru>
2024-08-31 20:28:17 +0500

    Fixing the error of the missing shell section in the path. Move images

--- FastCloneSetup.ps1
+++ FastCloneSetup.ps1
@@ -3,10 +3,14 @@ param (
 )
 
 $CallPath = Get-Location
+
+if (-not (Test-Path "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell")) {
+    New-Item "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell"
+}
 New-Item "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone"
 if ($Neko -eq "neko") {
-    New-ItemProperty "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone" -Name "Icon" -Value "`"$CallPath\NekoArk.ico`""
+    New-ItemProperty "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone" -Name "Icon" -Value "`"$CallPath\images\NekoArk.ico`""
 } else {
-    New-ItemProperty "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone" -Name "Icon" -Value "`"$CallPath\FastClone.ico`""
+    New-ItemProperty "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone" -Name "Icon" -Value "`"$CallPath\images\FastClone.ico`""
 }
 New-Item "Registry::HKCU\SOFTWARE\Classes\Directory\Background\shell\Fast Clone\command" -Value "powershell.exe -ExecutionPolicy Bypass -File `"$CallPath\FastClone.ps1`""
